823b867dab28e5b9f66b297fb05808f478e1995a
Abdallah229 <11410120230231@stud.cu.edu.eg>
2024-07-06 02:53:37 +0300

    commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Lenovo/CLionProjects/3_IN_1
+# Install script for directory: D:/Git Hub/FCAI/Year 1/3-IN-1 Game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Lenovo/CLionProjects/3_IN_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Git Hub/FCAI/Year 1/3-IN-1 Game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
